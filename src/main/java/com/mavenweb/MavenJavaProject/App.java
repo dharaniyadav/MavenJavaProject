3613c32c5e981b6ce2a106d12a34406cc22cc4f6
dharaniyadav <dharaniyadav2002@gmail.com>
2022-11-16 13:13:48 +0530

    Maven java project push

---
+++ src/main/java/com/mavenweb/MavenJavaProject/App.java
@@ -0,0 +1,13 @@
+package com.mavenweb.MavenJavaProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World from M Dharani-20BD1A6686-CSM(B)!" );
+    }
+}
